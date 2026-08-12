9efeb606aca321e0e100b8c3d24fc6f401565d64
maharaj-99 <maharajayon@gmail.com>
2026-08-13 03:06:56 +0600

    js add

---
+++ script.js
@@ -0,0 +1,20 @@
+var icon = document.querySelector(".icons");
+var navLinks = document.querySelector(".nav-links");
+var bars = document.querySelector(".fa-bars");
+var xMark = document.querySelector(".fa-xmark");
+
+
+function clickNow() {
+
+    navLinks.classList.toggle("show");
+
+    if(navLinksclassList.contains("show")){
+        
+        bars.style.display = "none";
+        xMark.style.display = "block";
+    }
+    else {
+        bars.style.display = "block";
+        xMark.style.display = "none";
+    }
+};
